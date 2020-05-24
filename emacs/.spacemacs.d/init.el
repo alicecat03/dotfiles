a3bcd3a2908f499519328e6b051baa4af8366ca0
iwaizm <iwaizm@protonmail.com>
2020-05-24 09:43:15 +0900

    .ddskkを編集

--- emacs/.spacemacs.d/init.el
+++ emacs/.spacemacs.d/init.el
@@ -33,6 +33,7 @@
    '(
      html
      python
+     twitter
      ;; ----------------------------------------------------------------
      ;; Example of useful layers you may want to use right away.
      ;; Uncomment some layer names and press <SPC f e R> (Vim style) or
