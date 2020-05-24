58e47ea04dae67356202b164343d6df897d7298d
iwaizm <iwaizm@protonmail.com>
2020-05-24 14:31:57 +0900

    migemo の設定を削除

--- emacs/.spacemacs.d/user-init.el
+++ emacs/.spacemacs.d/user-init.el
@@ -26,3 +26,19 @@
 ;; [[file:~/dotfiles/emacs/.spacemacs.d/spacemacs.org::*%E8%A8%AD%E5%AE%9A%E3%83%95%E3%82%A1%E3%82%A4%E3%83%AB%E3%82%92%20=~/.ddskk=%20%E3%81%AB%E3%81%BE%E3%81%A8%E3%82%81%E3%82%8B][設定ファイルを =~/.ddskk= にまとめる:1]]
 (setq skk-user-directory "~/.ddskk")
 ;; 設定ファイルを =~/.ddskk= にまとめる:1 ends here
+
+;; [[file:~/dotfiles/emacs/.spacemacs.d/spacemacs.org::*%E6%96%B0%E3%81%97%E3%81%84%20EWW%20%E3%83%90%E3%83%83%E3%83%95%E3%82%A1%E3%81%AE%E5%90%8D%E5%89%8D%E3%82%92%E8%87%AA%E5%8B%95%E7%9A%84%E3%81%AB%E5%A4%89%E6%9B%B4][新しい EWW バッファの名前を自動的に変更:1]]
+(defun xah-rename-eww-hook ()
+  "Rename eww browser's buffer so sites open in new page."
+  (rename-buffer "eww" t))
+(add-hook 'eww-mode-hook #'xah-rename-eww-hook)
+
+(defun modi/force-new-eww-buffer (orig-fun &rest args)
+  "When prefix argument is used, a new eww buffer will be created,
+regardless of whether the current buffer is in `eww-mode'."
+  (if current-prefix-arg
+      (with-temp-buffer
+        (apply orig-fun args))
+    (apply orig-fun args)))  
+(advice-add 'eww :around #'modi/force-new-eww-buffer)
+;; 新しい EWW バッファの名前を自動的に変更:1 ends here
